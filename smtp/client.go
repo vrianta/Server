c1adb9f8d3494d930449aaca128f9e0f4e7d2000
tutoraddicts <rohitdutta907@gmail.com>
2025-01-22 04:23:32 +0530

    for user session we will create new client and new connection

--- smtp/client.go
+++ smtp/client.go
@@ -12,9 +12,8 @@ type sMTPConfig struct {
 }
 
 type sMTPClient struct {
-	sMTPConfig *sMTPConfig
-	auth       smtp.Auth
-	address    string
+	auth    smtp.Auth
+	address string
 
 	host               string
 	port               int
@@ -30,13 +29,19 @@ func (s *sMTPClient) InitSMTPClient(host string, port int, username, password st
 	s.port = port
 	s.username = username
 	s.password = password
+	s.auth = smtp.PlainAuth("", username, password, host)
+
+	return nil
+}
+
+func (s *sMTPClient) SendMail(to []string, subject, body string) error {
 
 	conn, err := net.Dial("tcp", s.address)
 	if err != nil {
 		return err
 	}
 
-	client, err := smtp.NewClient(conn, host)
+	client, err := smtp.NewClient(conn, s.host)
 	if err != nil {
 		fmt.Printf("failed to create client : %s", err.Error())
 		return err
@@ -48,35 +53,21 @@ func (s *sMTPClient) InitSMTPClient(host string, port int, username, password st
 		return err
 	}
 
-	s.auth = smtp.PlainAuth("", username, password, host)
 	if err := client.Auth(s.auth); err != nil {
 		fmt.Printf("failed to Authenticate client : %s", err.Error())
 		return err
 	}
 
-	s.sMTPConfig = &sMTPConfig{client: client}
-
-	return nil
-}
-
-func (s *sMTPClient) SendMail(to []string, subject, body string) error {
-
-	if s.sMTPConfig.client.Noop() != nil {
-		fmt.Printf("failed connection reestablishing")
-		if err := s.InitSMTPClient(s.host, s.port, s.username, s.password); err != nil {
-			return err
-		}
-	}
-	if err := s.sMTPConfig.client.Mail(s.username); err != nil {
+	if err := client.Mail(s.username); err != nil {
 		return err
 	}
 
 	for _, recipient := range to {
-		if err := s.sMTPConfig.client.Rcpt(recipient); err != nil {
+		if err := client.Rcpt(recipient); err != nil {
 			return err
 		}
 	}
-	writer, err := s.sMTPConfig.client.Data()
+	writer, err := client.Data()
 	if err != nil {
 		return err
 	}
@@ -85,10 +76,8 @@ func (s *sMTPClient) SendMail(to []string, subject, body string) error {
 	message := fmt.Sprintf("From: %s\r\nSubject: %s\r\n\r\n%s", s.username, subject, body)
 	_, err = writer.Write([]byte(message))
 
+	client.Close()
+
 	return err
 
-}
-
-func (s *sMTPClient) Close() error {
-	return s.sMTPConfig.client.Quit()
 }
